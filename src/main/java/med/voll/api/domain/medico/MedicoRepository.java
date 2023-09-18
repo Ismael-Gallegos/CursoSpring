80c12d3c89f8f03140115cad8004db318a99b15f
Ismael Gallegos <fcgg241007@outlook.com>
2023-09-17 22:08:40 -0700

    Primer commit desde Intelijj

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
-
+// Primer commit desde Intellij
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findByActivoTrue(Pageable paginacion);
 
