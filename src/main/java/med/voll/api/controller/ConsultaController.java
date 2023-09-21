31f36a3af907fedee4a84e496ecbad319a25fee2
Ismael Gallegos <fcgg241007@outlook.com>
2023-09-21 15:46:50 -0700

    Agregados los siguientes cambios:
    1. Se añadió la dependencia de Springdoc OpenAPI en la versión 2.2.0 al proyecto.
    
    2. Se Sobreescribio las propiedades del archivo application.properties, creando otro archivo llamado application-test.properties que se carga solo cuando se ejecutan pruebas, usando la anotación @ActiveProfiles para eso.
    
    3. Se realizaron pruebas unitarias en `ConsultaControllerTest` para verificar el funcionamiento de la cancelación de consultas.
    
    4. Se añadió la anotación `@SecurityRequirement(name = "bearer-key")` en los controladores `ConsultaController`, `MedicoController`, y `PacienteController`.
    
    5. Se creó el archivo `SpringDocConfigurations` para la configuración personalizada de Springdoc OpenAPI.
    
    6. En `MedicoRepositoryTest`, se realizaron pruebas de selección de médicos con especialidad en una fecha determinada.
    
    7. Se agregó el controlador para cancelar una consulta en `ConsultaController`.
    
    8. Se Probaron los escenarios de error 400 y código 200 al probar una clase de controlador.

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -1,24 +1,23 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consulta.AgendaDeConsultaService;
 import med.voll.api.domain.consulta.DatosAgendarConsulta;
-import med.voll.api.domain.consulta.DatosDetalleConsulta;
-import med.voll.api.domain.paciente.DatosDetallesPaciente;
+import med.voll.api.domain.consulta.DatosCancelamientoConsulta;
+import med.voll.api.infra.errores.ValidacionDeIntegridad;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
 @RequestMapping("/consultas")
 @SecurityRequirement(name = "bearer-key")
+@SuppressWarnings("all")
 public class ConsultaController {
 
     @Autowired
@@ -26,9 +25,24 @@ public class ConsultaController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity agendar(@RequestBody @Valid DatosAgendarConsulta datos) {
-
+    @Operation(
+            summary = "registra una consulta en la base de datos",
+            description = "",
+            tags = { "consulta", "post" })
+    public ResponseEntity agendar(@RequestBody @Valid DatosAgendarConsulta datos) throws ValidacionDeIntegridad {
         var response = service.agendar(datos);
         return ResponseEntity.ok(response);
     }
+
+    @DeleteMapping
+    @Transactional
+    @Operation(
+            summary = "cancela una consulta de la agenda",
+            description = "requiere motivo",
+            tags = { "consulta", "delete" })
+    public ResponseEntity cancelar(@RequestBody @Valid DatosCancelamientoConsulta dados) throws ValidacionDeIntegridad {
+        service.cancelar(dados);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/med/voll/api/domain/consulta/DatosAgendarConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DatosAgendarConsulta.java
@@ -4,8 +4,16 @@ import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import med.voll.api.domain.medico.Especialidad;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public record DatosAgendarConsulta(Long id, @NotNull Long idPaciente, Long idMedico, @NotNull @Future LocalDateTime fecha, Especialidad especialidad) {
+public record DatosAgendarConsulta(
+
+        @NotNull
+        Long idPaciente,
+        Long idMedico,
+        @NotNull
+        @Future
+        LocalDateTime fecha,
+        Especialidad especialidad) {
+
 }
